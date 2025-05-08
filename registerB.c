51dcf7c6b7751b70f06756d27971c70c0fc3fc5e
Lenilson A. Santos <savelino852@gmail.com>
2025-05-08 15:15:41 -0300

    Update registerB.c

--- registerB.c
+++ registerB.c
@@ -2,16 +2,23 @@
 #include <avr/interrupt.h>
 #include "registerB.h"
 
+static volatile uint8_t pwm = 0x00;
+static volatile int direcao = 0x01;
+
 // Implementação do setup
 void setup(void) {
-     DDRB &=  ~( 0x01 << PB5 );
-     DDRB |=  ( !0x00 << PB5 );
-     PORTB &= ~( 0x01 << PB5 );
+     DDRB |= (( !0x00 << PB5 ) | ( !0x00 << PB4 ));
+     PORTB &= ~(( 0x01 << PB5 ) | ( 0x01 << PB4 ));
 
     TCCR1A = 0x00; // Modo normal
-    TCCR1B = (1 << WGM12) | (1 << CS12) | (0 << CS11) | (1 << CS10); // CTC, prescaler 64
-    OCR1A = 15624 >> 1; // Conta até 249 (1ms) para calcular o segundo multiplicar com 1000
-    TIMSK1 |= (1 << OCIE1A); // Habilita interrupção de comparação A
+    TCCR1B = (0x01 << WGM12) | (0x01 << CS12) | (0x00 << CS11) | (0x01 << CS10); // CTC, prescaler 64
+    OCR1A = 0x100; // Conta até 256 (?) para calcular o ? multiplicar com ?
+    TIMSK1 |= (0x01 << OCIE1A); // Habilita interrupção de comparação A
+
+    // --- Configura Timer2 para Fast PWM no pino OC2B ---
+    TCCR2A = (0x01 << COM2B1) | (0x01 << WGM21) | (0x01 << WGM20); // Fast PWM, não-invertido
+    TCCR2B = (0x01 << CS21); // Prescaler = 8
+    OCR2B = pwm;
 }
 
 // Implementação do loop
@@ -21,7 +28,8 @@ void loop(void) { }
 ISR(TIMER1_COMPA_vect) {
     static uint8_t timeCount = 0x00;
     ++timeCount;
-    if (timeCount == 0xCB) {
-        PORTB_REG.pb5 ^= 0x01;
-    }
+    if (timeCount == 0xCB) { PORTB_REG.pb5 ^= 0x01; timeCount = 0x00; }
+    pwm += direcao;
+    if (pwm == 0xff || pwm == 0x00) { direcao = -direcao; /* Inverte direção quando atinge os limites */ }
+    OCR2B = pwm;
 }
