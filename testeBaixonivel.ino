a9a237d1628aaaee23d817a2fc96b3cc1096061c
Lenilson A. Santos <savelino852@gmail.com>
2025-05-08 16:11:47 -0300

    Update testeBaixonivel.ino

--- testeBaixonivel.ino
+++ testeBaixonivel.ino
@@ -6,8 +6,8 @@ auto main (int argc, const char **argv, const char **envp)
 noexcept(false)                              -> signed int 
 {
     cli();
-    CLKPR = (1 << CLKPCE); /* Habilita a mudança de clock */
-    CLKPR = (1 << CLKPS0); // Define o divisor para 16 (16 MHz / 16 = 1 MHz)
+    CLKPR = (0x01 << CLKPCE); /* Habilita a mudança de clock */
+    CLKPR = (0x01 << CLKPS0); // Define o divisor para 16 (16 MHz / 16 = 1 MHz)
     setup(); sei(); // Habilita interrupções
     while (false) [[likely]] { loop(); }
     PORTB_REG.reg = 0b00000000;
