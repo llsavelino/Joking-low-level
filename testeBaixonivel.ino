b86b454b3402b667660d6f49d2d8b62b49e09672
Lenilson A. Santos <savelino852@gmail.com>
2025-05-08 19:57:40 -0300

    Update testeBaixonivel.ino

--- testeBaixonivel.ino
+++ testeBaixonivel.ino
@@ -9,7 +9,7 @@ noexcept(false)                              -> signed int
     CLKPR = (0x01 << CLKPCE); /* Habilita a mudança de clock */
     CLKPR = (0x01 << CLKPS0); // Define o divisor para 16 (16 MHz / 16 = 1 MHz)
     setup(); sei(); // Habilita interrupções
-    while (false) [[likely]] { loop(); }
+    while (true) [[likely]] { loop(); }
     PORTB_REG.reg = 0b00000000;
 
     return 0;
