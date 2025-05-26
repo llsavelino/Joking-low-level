01a5d2bd8c62b19b42ac6c626130781cf0e4d79d
Lenilson A.S <savelino852@gmail.com>
2025-05-26 08:28:15 -0300

    Update registerB.c

--- registerB.c
+++ registerB.c
@@ -32,6 +32,12 @@ static operatingSystem tasks[NUM_TASKS] = {
 #else 
     #warning NUM_TASKS não definida... Isso pode resultar em erro.
 #endif
+// Caixa de status do sistema...
+const char* monitor[3][2] = {
+    {"Inicializando OS.\n\0", "Configurando timer 1, do atmega328p. \n.\0"},
+    {"Abilitando flag no ciclo de interupição. \n\0", "Chamando tarefa. \n\0"},
+    {"Executando... \n\0", "Encerando OS. \r\t\n\0"}
+};
 #if defined(PORTB_REG) || defined(PORTC_REG) || defined(PORTD_REG)
     #warning Tudo ok, hardware mapeado via software. Cuidado isso pode falhar!!
 void setup(void) {
