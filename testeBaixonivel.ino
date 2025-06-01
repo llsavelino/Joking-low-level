9ae84225f177206594b4d699e36e2f6a02dc4629
Lenilson A.S <savelino852@gmail.com>
2025-06-01 11:40:16 -0300

    Update testeBaixonivel.ino

--- testeBaixonivel.ino
+++ testeBaixonivel.ino
@@ -1,16 +1,17 @@
-#include        <avr/io.h>
-#include <avr/interrupt.h>
-#include     "registerB.h"
+#include                                        <avr/io.h>
+#include                                 <avr/interrupt.h>
+#include /*     Mapa de registradores     */ "registerB.h"
 
 auto main (int argc, const char **argv, const char **envp) 
-noexcept(false)                              -> signed int 
+->                                              signed int 
 {
-    cli();
-    CLKPR = (0x01 << CLKPCE); /* Habilita a mudança de clock */
-    CLKPR = (0x01 << CLKPS0); // Define o divisor para 16 (16 MHz / 16 = 1 MHz)
-    setup(); sei(); // Habilita interrupções
-    while (true) [[likely]] { loop(); }
-    PORTB_REG.reg = 0b00000000;
+    cli(); /* Habilita a mudança de clock */       CLKPR =
+    (0x01                                      << CLKPCE);
+    CLKPR =                              (0x01 << CLKPS0); 
+    // Define o divisor para 16      (16 MHz / 16 = 1 MHz)
+    setup(); /* Habilita interrupções */            sei(); 
+    while (true) [[likely]]                    { loop(); }
+    PORTB_REG.reg                            = 0b00000000;
 
-    return 0;
+    return (0x00)                   ? -+(0x01) : -+(0x00);
 }
