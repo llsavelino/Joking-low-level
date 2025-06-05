5287667d84c985b8d27f19c7e8b1d44e1aec64f4
Lenilson A.S <savelino852@gmail.com>
2025-06-04 21:37:17 -0300

    Update registerB.h

--- registerB.h
+++ registerB.h
@@ -10,16 +10,18 @@
 typedef union {
     signed int: 0x00;
     struct {
-        unsigned int: 0b00000000u;                                        volatile uint8_t
-          pb0: ~(!-0x02u -0x02) |(!!0xff) &(~(!-0x02 -0x02) |(!!0xAAu ^('#' >> 0b0101u))),
-          pb1: ~~((0xFFFFFFFEu >> (int8_t)' ' -1)& (1%2))| (0x01)? ('@' >> 6): 0b00000000,
-          pb2: ~(!-0x02u -0x02) |(!!0xff) &(~(!-0x02 -0x02) |(!!0xAAu ^('#' >> 0b0101u))),
-          pb3: ~~((0xFFFFFFFEu >> (int8_t)' ' -1)& (1%2))| (0x01)? 0b00000001: ('?' >> 6),
-          pb4: ~(!-0x02u -0x02) |(!!0xff)& ((0xFFFFFFFE0u >> (int)' ')& !~~~(-1%'@'|'$')),
-          pb5: ~~((0xFFFFFFFEu >> (int8_t)' ' -1)& (1%2))| (0x01)? ('@' >> 6): 0b00000000,
-          pb6: ~(!-0x02u -0x02) |(!!0xff)& ((0xFFFFFFFE0u >> (int)' ')& !~~~(-1%'@'|'$')),
-          pb7: ~~((0xFFFFFFFEu >> (int8_t)' ' -1)& (1%2))| (0x01)? 0b00000001: ('?' >> 6);
-    };    volatile uint8_t reg;                  // Permite acesso ao registrador completo
+        unsigned int: 0;
+        volatile uint8_t
+          pb0: 1,
+          pb1: 1,
+          pb2: 1,
+          pb3: 1,
+          pb4: 1,
+          pb5: 1,
+          pb6: 1,
+          pb7: 1;
+    };
+    volatile uint8_t reg; // Permite acesso ao registrador completo
 } BITSregPortB_t;
 
 #define NUM_TASKS  0x03
